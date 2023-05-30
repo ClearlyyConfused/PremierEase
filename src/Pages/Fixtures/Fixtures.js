92717dbd3bfc79e787bf19a832501c8fa4214826
Louis Doan <louisdoan9@gmail.com>
2023-05-30 18:33:13 -0400

    Have fixtures and results be divided by date

--- src/Pages/Fixtures/Fixtures.js
+++ src/Pages/Fixtures/Fixtures.js
@@ -1,37 +1,50 @@
 import './Fixtures.css';
+import SortFixtures from './SortFixtures';
 
 function Fixtures({ leagueFixtures }) {
+	let matches = leagueFixtures !== undefined ? SortFixtures(leagueFixtures) : [];
+
 	if (leagueFixtures === undefined) {
 		return <div>Loading...</div>;
 	} else {
 		return (
 			<table className="league-fixtures">
-				{leagueFixtures.map((fixture) => {
-					// only returns upcoming matches
-					if (fixture.status === 'SCHEDULED' || fixture.status === 'TIMED') {
-						console.log(fixture);
-						return (
-							<tr>
-								<td>{fixture.awayTeam.shortName}</td>
-								<td>
-									<img src={fixture.awayTeam.crest} alt={fixture.awayTeam.shortName} />
-								</td>
-								<td>
-									{new Date(fixture.utcDate).toLocaleString('en-US', {
-										year: 'numeric',
-										month: 'numeric',
-										day: 'numeric',
-										hour: 'numeric',
-										minute: 'numeric',
-									})}
-								</td>
-								<td>
-									<img src={fixture.homeTeam.crest} alt={fixture.homeTeam.shortName} />
-								</td>
-								<td>{fixture.homeTeam.shortName}</td>
-							</tr>
-						);
-					}
+				{matches.map((dateMatchesPair) => {
+					// only returns finished matches
+					return (
+						<tr>
+							<h2>{dateMatchesPair[0]}</h2>
+							{dateMatchesPair[1].reverse().map((fixture) => {
+								return (
+									<tr>
+										<td>{fixture.awayTeam.shortName}</td>
+										<td>
+											<img
+												src={fixture.awayTeam.crest}
+												alt={fixture.awayTeam.shortName}
+											/>
+										</td>
+										<td>
+											{new Date(fixture.utcDate).toLocaleString('en-US', {
+												year: 'numeric',
+												month: 'numeric',
+												day: 'numeric',
+												hour: 'numeric',
+												minute: 'numeric',
+											})}
+										</td>
+										<td>
+											<img
+												src={fixture.homeTeam.crest}
+												alt={fixture.homeTeam.shortName}
+											/>
+										</td>
+										<td>{fixture.homeTeam.shortName}</td>
+									</tr>
+								);
+							})}
+						</tr>
+					);
 				})}
 			</table>
 		);
--- src/Pages/Results/Results.js
+++ src/Pages/Results/Results.js
@@ -1,30 +1,43 @@
 import './Results.css';
+import SortResults from './SortResults';
 
 function Results({ leagueFixtures }) {
+	let matches = leagueFixtures !== undefined ? SortResults(leagueFixtures) : [];
+
 	if (leagueFixtures === undefined) {
 		return <div>Loading...</div>;
 	} else {
 		return (
 			<table className="league-results">
-				{/* reversed to display latest matches first */}
-				{[...leagueFixtures].reverse().map((fixture) => {
+				{matches.map((dateMatchesPair) => {
 					// only returns finished matches
-					if (fixture.status === 'FINISHED') {
-						return (
-							<tr>
-								<td>{fixture.awayTeam.shortName}</td>
-								<td>
-									<img src={fixture.awayTeam.crest} alt={fixture.awayTeam.shortName} />
-								</td>
-								<td>{fixture.score.fullTime.away}</td>
-								<td>{fixture.score.fullTime.home}</td>
-								<td>
-									<img src={fixture.homeTeam.crest} alt={fixture.homeTeam.shortName} />
-								</td>
-								<td>{fixture.homeTeam.shortName}</td>
-							</tr>
-						);
-					}
+					return (
+						<tr>
+							<h2>{dateMatchesPair[0]}</h2>
+							{dateMatchesPair[1].reverse().map((fixture) => {
+								return (
+									<tr>
+										<td>{fixture.awayTeam.shortName}</td>
+										<td>
+											<img
+												src={fixture.awayTeam.crest}
+												alt={fixture.awayTeam.shortName}
+											/>
+										</td>
+										<td>{fixture.score.fullTime.away}</td>
+										<td>{fixture.score.fullTime.home}</td>
+										<td>
+											<img
+												src={fixture.homeTeam.crest}
+												alt={fixture.homeTeam.shortName}
+											/>
+										</td>
+										<td>{fixture.homeTeam.shortName}</td>
+									</tr>
+								);
+							})}
+						</tr>
+					);
 				})}
 			</table>
 		);
