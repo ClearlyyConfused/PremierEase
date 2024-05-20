4aa502434f25e0225eb15feac2d147333c089f72
Louis Doan <louisdoan9@gmail.com>
2024-05-19 21:46:35 -0400

    Add tests for using the club & date filter at once

--- src/Tests/endToEnd.test.js
+++ src/Tests/endToEnd.test.js
@@ -1,6 +1,6 @@
 const { By, Builder, Browser, until } = require('selenium-webdriver');
 
-describe.skip('Test general navigation of the site using the nav bar', () => {
+describe('Test general navigation of the site using the nav bar', () => {
 	let driver;
 	beforeAll(async () => {
 		driver = await new Builder().forBrowser(Browser.CHROME).build();
@@ -39,7 +39,7 @@ describe.skip('Test general navigation of the site using the nav bar', () => {
 	});
 });
 
-describe.skip('Test general navigation of the mobile layout of the site using the nav bar', () => {
+describe('Test general navigation of the mobile layout of the site using the nav bar', () => {
 	let driver;
 	beforeAll(async () => {
 		driver = await new Builder().forBrowser(Browser.CHROME).build();
@@ -164,9 +164,43 @@ describe('Test filters on "Fixtures" page', () => {
 			).toBeTruthy();
 		}
 	});
+
+	test('Setting club and starting/ending date filter only displays fixtures that include that club between those corresponding dates', async () => {
+		await driver
+			.wait(until.elementLocated(By.className('team-filter-container')))
+			.then(async (e) => await e.click());
+
+		const teams = await driver
+			.findElement(By.className('team-filter-dropdown'))
+			.findElements(By.xpath('.//div'));
+		const x = Math.floor(Math.random() * (teams.length - 1)) + 1;
+		const filteredTeamName = await teams[x].getText();
+		await teams[x].click();
+
+		await driver
+			.wait(until.elementLocated(By.id('start-date')))
+			.then(async (e) => await e.sendKeys('01-01-' + currentYear));
+		await driver
+			.wait(until.elementLocated(By.id('end-date')))
+			.then(async (e) => await e.sendKeys('04-01-' + currentYear));
+
+		const matches = await driver.findElements(By.className('matches-on-date'));
+		for (const match of matches) {
+			const teams = await match.findElements(By.xpath('.//tr/div/td'));
+			const team1 = await teams[0].getText();
+			const team2 = await teams[4].getText();
+			expect([team1, team2]).toContain(filteredTeamName);
+
+			const matchDate = await match.findElement(By.css('h2')).then((e) => e.getText());
+			expect(
+				new Date('01-01-' + currentYear) <= new Date(matchDate) &&
+					new Date(matchDate) <= new Date('04-01-' + currentYear)
+			).toBeTruthy();
+		}
+	});
 });
 
-describe.skip('Test filters on "Results" page', () => {
+describe('Test filters on "Results" page', () => {
 	let driver;
 	let currentYear = new Date().getFullYear();
 
@@ -179,7 +213,7 @@ describe.skip('Test filters on "Results" page', () => {
 		await driver.quit();
 	});
 
-	test.skip('Setting club filter to a club displays only results that contain that club', async () => {
+	test('Setting club filter to a club displays only results that contain that club', async () => {
 		await driver
 			.wait(until.elementLocated(By.className('team-filter-container')))
 			.then(async (e) => await e.click());
@@ -238,6 +272,40 @@ describe.skip('Test filters on "Results" page', () => {
 			expect(
 				new Date('01-15-' + currentYear) <= new Date(matchDate) &&
 					new Date(matchDate) <= new Date('03-15-' + currentYear)
+			).toBeTruthy();
+		}
+	});
+
+	test('Setting club and starting/ending date filter only displays results that include that club between those corresponding dates', async () => {
+		await driver
+			.wait(until.elementLocated(By.className('team-filter-container')))
+			.then(async (e) => await e.click());
+
+		const teams = await driver
+			.findElement(By.className('team-filter-dropdown'))
+			.findElements(By.xpath('.//div'));
+		const x = Math.floor(Math.random() * (teams.length - 1)) + 1;
+		const filteredTeamName = await teams[x].getText();
+		await teams[x].click();
+
+		await driver
+			.wait(until.elementLocated(By.id('start-date')))
+			.then(async (e) => await e.sendKeys('01-01-' + currentYear));
+		await driver
+			.wait(until.elementLocated(By.id('end-date')))
+			.then(async (e) => await e.sendKeys('04-01-' + currentYear));
+
+		const matches = await driver.findElements(By.className('matches-on-date'));
+		for (const match of matches) {
+			const teams = await match.findElements(By.xpath('.//tr/div/td'));
+			const team1 = await teams[0].getText();
+			const team2 = await teams[4].getText();
+			expect([team1, team2]).toContain(filteredTeamName);
+
+			const matchDate = await match.findElement(By.css('h2')).then((e) => e.getText());
+			expect(
+				new Date('01-01-' + currentYear) <= new Date(matchDate) &&
+					new Date(matchDate) <= new Date('04-01-' + currentYear)
 			).toBeTruthy();
 		}
 	});
